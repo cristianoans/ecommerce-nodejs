3f46dcd0bdc445c8bc0ba8a3a3226eaa7b16f930
unknown <cristianoans@hotmail.com>
2023-04-24 17:03:02 -0300

    metodo delete finalizado

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const produtos = express.Router();
 const Produto = require('../model/Produto');
+const { errorMonitor } = require('events');
 
 produtos.route('/')
     .get(async (req, res) => {
-        res.json({ mensagem: 'rota get' })
+
+        try {
+            const response = await Produto.find();
+            res.status(200).json(response);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+
     })
     .post(async (req, res) => {
         const { nome, descricao, quantidade, preco, desconto, dataDesconto, categoria, imgProduto } = req.body;
@@ -31,7 +39,17 @@ produtos.route('/')
         res.json({ message: "rota put" })
     })
     .delete(async (req, res) => {
-        res.json({ mensagem: 'rota delete' })
+        const { id } = req.body;
+        try {
+            const response = await Produto.findByIdAndRemove(id);
+            if (!response) {
+                return res.status(404).json({ mensagem: "produto não encontrado" });
+            }
+            res.status(200).json(response);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     });
 
 module.exports = produtos;
+
